1c002f504936ad0f3f171726d5273f81715bfe93
Đoàn Hữu Toàn <25521869@gm.uit.edu.vn>
2026-04-30 20:33:59 +0700

    Update cARRAY_H.h

--- cARRAY_H.h
+++ cARRAY_H.h
@@ -1,22 +1,36 @@
 #ifndef CARRAY_H
 #define CARRAY_H
+
 #include <iostream>
+#include <vector>
 #include <algorithm>
+#include <cmath>
 using namespace std;
 class cArray{
 private:
     int n;
     int *a;
+    int *backup;
 public:
-    cArray();
+    cArray(int size = 0);
     ~cArray();
     void Nhap();
     void Xuat();
+    void Reset();
+    int getSize() { return n; }
     int DemX(int x);
     bool KiemTraTangDan();
     int TimLeMin();
+    bool laSNT(int n);
     int TimSNTMax();
-    void SapXepTang(); 
+    void InsertionSort();
+    void MergeSort(int l, int r);
+    void QuickSort(int l, int r);
+    void HeapSort();
+    void CountingSort();
+    void RadixSort();
+    void merge(int l, int m, int r);
+    void heapify(int n, int i);
+    int getMax();
 };
-bool laSNT(int n);
 #endif
